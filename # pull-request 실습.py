e6a189773bbb018386f1f95c65aad3be6797526e
choms218 <81406135+choms218@users.noreply.github.com>
2023-03-17 13:31:51 +0900

    Add files via upload

---
+++ # pull-request 실습.py
@@ -0,0 +1,8 @@
+# pull-request 실습
+
+a = int(input('날짜를 입력하세요(월): '))
+b = int(input('날짜를 입력하세요(일): '))
+c = int(input('시간을 입력하세요(시): '))
+d = int(input('시간을 입력하세요(분): '))
+
+print('오늘은 {0}월 {1}일 이고, 현재시각은 {2}시 {3}분 입니다'.format(a,b,c,d))
